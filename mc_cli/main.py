32f0ac89e58ade8cf3058ffd26512601924c9969
flashgnash <flashgnash@proton.me>
2024-07-23 16:58:58 +0100

    Correct filenames on download

--- mc_cli/main.py
+++ mc_cli/main.py
@@ -2,31 +2,37 @@ import json
 import requests
 import os
 import argparse
+from tqdm import *
 
 # Define constants
 CURSEFORGE_API_BASE = "https://curseforge.com/api/v1"
 
+SERVER_PACK_BASE = "output/"
+
+
 def format_forge_download_uri(minecraft_ver,forge_ver):
-    return f"https://maven.minecraftforge.net/net/minecraftforge/forge/{minecraft_ver}-{forge_ver}/forge-{minecraft_ver}-{forge_ver}-installer.jarDOWNLOAD_DIR = 'downloads'"
+    return f"https://maven.minecraftforge.net/net/minecraftforge/forge/{minecraft_ver}-{forge_ver}/forge-{minecraft_ver}-{forge_ver}-installer.jar'"
 
 # Function to download a file from CurseForge
-def download_file(project_id, file_id):
+def download_file(mods_path,project_id, file_id):
     # Construct the URL to download the file
     download_url = f"{CURSEFORGE_API_BASE}/mods/{project_id}/files/{file_id}/download"
     
     # Create the directory if it doesn't exist
-    if not os.path.exists(DOWNLOAD_DIR):
-        os.makedirs(DOWNLOAD_DIR)
+    if not os.path.exists(mods_path):
+        os.makedirs(mods_path)
     
     # Send a GET request to download the file
-    response = requests.get(download_url)
+    response = requests.get(download_url,allow_redirects = True)
     
     if response.status_code == 200:
-        file_path = os.path.join(DOWNLOAD_DIR, f"{project_id}_{file_id}.jar")
+        final_url = response.url;        
+        filename = final_url.split("/")[-1]
+
+        file_path = os.path.join(mods_path, filename)
         with open(file_path, 'wb') as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
-        print(f"Downloaded {file_path}")
     else:
         print(f"Failed to download file {download_url}: {response.status_code}")
 
@@ -57,11 +63,11 @@ def main():
     files = data.get('files', [])
     
     # Download each file
-    for file_info in files:
+    for file_info in tqdm(files,desc="Downloading mods... "):
         project_id = file_info.get('projectID')
         file_id = file_info.get('fileID')
         if project_id and file_id:
-            download_file(project_id, file_id)
+            download_file("test",project_id, file_id)
         else:
             print(f"Invalid file information: {file_info}")
 
